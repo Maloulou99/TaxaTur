8909030a5d7deb21348344e23ea7cec958720233
Malou <baha0612@stud.kea.dk>
2023-08-28 21:41:33 +0200

    Ændret til mere simpel kode

--- src/main/java/org/example/TaxiFareCalculator.java
+++ src/main/java/org/example/TaxiFareCalculator.java
@@ -4,64 +4,79 @@ import java.util.Scanner;
 
 
     public class TaxiFareCalculator {
-        private double dayTime = 39;
-        private double eveningTime = 49;
-        private double dayKmRate = 10;
-        private double eveningKmRate = 14;
+            private double dayTime = 39;
+            private double eveningTime = 49;
+            private double dayKmRate = 10;
+            private double eveningKmRate = 14;
 
-        public static void main(String[] args) {
-            TaxiFareCalculator fareCalculator = new TaxiFareCalculator();
-            fareCalculator.run();
-        }
+            public TaxiFareCalculator() {
 
-        public void run() {
-            Scanner scanner = new Scanner(System.in);
-
-            boolean calculateAgain = true;
-            while (calculateAgain) {
-                System.out.println("Taxi beregner:\n");
-
-                System.out.print("Hvor lang er taxa turen (km): ");
-                double distanceKm = scanner.nextDouble();
-
-                System.out.print("Skal du køre om dagen (06-18) [y/n] ? ");
-                char timeOfDayChoice = scanner.next().charAt(0);
-                boolean isEveningWeekend = (timeOfDayChoice == 'n' || timeOfDayChoice == 'N');
-
-                System.out.print("Antal passagere (maks 8)? ");
-                int passengers = scanner.nextInt();
-
-                System.out.print("Medbringes en cykel [y/n]? ");
-                char bikeChoice = scanner.next().charAt(0);
-                boolean hasBike = (bikeChoice == 'y' || bikeChoice == 'Y');
-
-                double fare = calculateFare(distanceKm, isEveningWeekend, passengers, hasBike);
-                System.out.println("\nTaxapris: " + fare + " kroner\n");
-
-                System.out.print("Lav en ny beregning [y/n]? ");
-                char newCalculationChoice = scanner.next().charAt(0);
-                calculateAgain = (newCalculationChoice == 'y' || newCalculationChoice == 'Y');
             }
 
-            System.out.println("Tak for brugen!");
-            scanner.close();
-        }
-
-        public double calculateFare(double distanceKm, boolean isEveningWeekend, int passengers, boolean hasBike) {
-            double baseFare = isEveningWeekend ? eveningTime : dayTime;
-            double kmRate = isEveningWeekend ? eveningKmRate : dayKmRate;
-
-            double fare = baseFare + distanceKm * kmRate;
-
-            if (passengers > 4) {
-                fare += 50;
+            public TaxiFareCalculator(double dayTime, double eveningTime, double dayKmRate, double eveningKmRate) {
+                this.dayTime = dayTime;
+                this.eveningTime = eveningTime;
+                this.dayKmRate = dayKmRate;
+                this.eveningKmRate = eveningKmRate;
             }
 
-            if (hasBike) {
-                fare += 25;
+            public static void main(String[] args) {
+                TaxiFareCalculator fareCalculator = new TaxiFareCalculator();
+                fareCalculator.run();
             }
 
-            return fare;
-        }
-    }
+            public void run() {
+                Scanner scanner = new Scanner(System.in);
 
+                boolean calculateAgain = true;
+                while (calculateAgain) {
+                    System.out.println("Taxi beregner:\n");
+
+                    System.out.print("Hvor lang er taxa turen (km): ");
+                    double distanceKm = scanner.nextDouble();
+
+                    System.out.print("Skal du køre om dagen (06-18) [y/n] ? ");
+                    boolean isEveningWeekend = scanner.next().equalsIgnoreCase("n");
+
+                    System.out.print("Antal passagere (maks 8)? ");
+                    int passengers = scanner.nextInt();
+
+                    System.out.print("Medbringes en cykel [y/n]? ");
+                    boolean hasBike = scanner.next().equalsIgnoreCase("y");
+
+                    double fare = calculateFare(distanceKm, isEveningWeekend, passengers, hasBike);
+                    System.out.println("\nTaxapris: " + fare + " kroner\n");
+
+                    System.out.print("Lav en ny beregning [y/n]? ");
+                    calculateAgain = scanner.next().equalsIgnoreCase("y");
+                }
+
+                System.out.println("Tak for brugen!");
+                scanner.close();
+            }
+
+            public static double calculateFare(double distanceKm, boolean isEveningWeekend, int passengers, boolean hasBike) {
+                double baseFare;
+                double kmRate;
+
+                if (isEveningWeekend) {
+                    baseFare = 49;
+                    kmRate = 14;
+                } else {
+                    baseFare = 39;
+                    kmRate = 10;
+                }
+
+                double fare = baseFare + distanceKm * kmRate;
+
+                if (passengers > 4) {
+                    fare += 50;
+                }
+
+                if (hasBike) {
+                    fare += 25;
+                }
+
+                return fare;
+            }
+        }
